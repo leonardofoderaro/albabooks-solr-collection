c6bda694b89b2296dc02254550ceeeb78c74d1ae
Leonardo Foderaro <iLeo@iLeo.local>
2015-09-02 16:22:49 +0200

    added shell vars, first step to improve configurability

--- upconfig.sh
+++ upconfig.sh
@@ -1,4 +1,8 @@
 
 PATH_TO_ZKCLI=/opt/solr/solr-5.2.1/server/scripts/cloud-scripts
 
-$PATH_TO_ZKCLI/zkcli.sh -cmd upconfig -zkhost localhost:9983 -confname albabooks -confdir ./solr
+# with Docker you need to overwrite these vars
+SOLRCLOUD_HOST=127.0.0.1
+ZKPORT=9983
+
+$PATH_TO_ZKCLI/zkcli.sh -cmd upconfig -zkhost $SOLRCLOUD_HOST:$ZKPORT -confname albabooks -confdir ./solr
