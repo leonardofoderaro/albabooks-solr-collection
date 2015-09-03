59be5e7f82bd76d52d781f7fdb0f288a80dfebc8
Leonardo Foderaro <leonardo.foderaro@gmail.com>
2015-09-03 09:51:09 +0200

    refactored and added some comments

--- bootstrapfromdocker.sh
+++ bootstrapfromdocker.sh
@@ -1,12 +1,19 @@
 
-gunzip albabooks.csv.gz
+# your local solrcloud installation, 
+# we need this to use zkcli.sh and bin/post
+SOLRCLOUD_PATH=/opt/solr/solr-5.2.1
 
-PATH_TO_ZKCLI=/opt/solr/solr-5.2.1/server/scripts/cloud-scripts
-
+# your Docker host, 
+# use  eval "$(docker-machine env default)" to get your correct address
 SOLRCLOUD_HOST=192.168.99.100
 
+# these params are used to map the containerized solrcloud ports with your host.
 SOLRCLOUD_PORT=9090
 ZKPORT=9091
+
+gunzip albabooks.csv.gz
+
+PATH_TO_ZKCLI=$SOLRCLOUD_PATH$/server/scripts/cloud-scripts
 
 docker run -dit -p $SOLRCLOUD_PORT:8983 -p $ZKPORT:9983 alba/latest
 
@@ -20,6 +27,6 @@ echo "creating the albabooks collection"
 wget -O - "http://$SOLRCLOUD_HOST:$SOLRCLOUD_PORT/solr/admin/collections?action=CREATE&name=albabooks&numShards=1&replicationFactor=1&collection.configName=albabooks"
 
 echo "loading the sample dataset"
-/opt/solr/solr-5.2.1/bin/post -host $SOLRCLOUD_HOST -port $SOLRCLOUD_PORT -c albabooks_shard1_replica1 albabooks.csv
+$SOLRCLOUD_PATH$/bin/post -host $SOLRCLOUD_HOST -port $SOLRCLOUD_PORT -c albabooks_shard1_replica1 albabooks.csv
 
 echo "Done!"
